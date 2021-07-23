fe25b617d3183fdfe896415de6f173e42bb4b14c
axelatc <axel.krebs@promsocatc.net>
2021-07-23 17:13:21 +0200

    Replace custom hashing and salting with Shiro's

--- src/main/java/com/atc/utils/AuthUtils.java
+++ src/main/java/com/atc/utils/AuthUtils.java
@@ -1,46 +1,17 @@
 package com.atc.utils;
 
 import org.apache.log4j.Logger;
-
-import javax.crypto.SecretKeyFactory;
-import javax.crypto.spec.PBEKeySpec;
-import java.nio.charset.StandardCharsets;
-import java.security.NoSuchAlgorithmException;
-import java.security.spec.InvalidKeySpecException;
-import java.security.spec.KeySpec;
-import java.util.Base64;
+import org.apache.shiro.authc.credential.DefaultPasswordService;
+import org.apache.shiro.authc.credential.PasswordService;
+import org.apache.shiro.env.BasicIniEnvironment;
+import org.apache.shiro.env.Environment;
+import org.apache.shiro.mgt.SecurityManager;
 
 public class AuthUtils {
     private final static Logger LOG = Logger.getLogger(AuthUtils.class);
-    private final static String HASH_AND_SALT_SEPARATOR = "++";
-    // it's unsecure to use a fixed salt but it's easier for PINTEG
-    private final static byte[] SALT = "A".getBytes(StandardCharsets.UTF_8);
-
 
     public final static String hashAndSalt(String password) {
-        // source: https://stackoverflow.com/questions/2860943/how-can-i-hash-a-password-in-java
-
-        LOG.info("Hashing and salting password...");
-/*        byte[] SALT = new byte[16];
-        Random random = new Random();
-        random.nextBytes(SALT);*/
-        KeySpec spec = new PBEKeySpec(password.toCharArray(), SALT, 65536, 128);
-        SecretKeyFactory f = null;
-        try {
-            f = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA1");
-        } catch (NoSuchAlgorithmException e) {
-            e.printStackTrace();
-        }
-        byte[] hash = new byte[0];
-        try {
-            hash = f.generateSecret(spec).getEncoded();
-        } catch (InvalidKeySpecException e) {
-            e.printStackTrace();
-        }
-        Base64.Encoder enc = Base64.getEncoder();
-
-        LOG.info("salt: " + enc.encodeToString(SALT));
-        LOG.info("hash: " + enc.encodeToString(hash));
-        return enc.encodeToString(hash) + HASH_AND_SALT_SEPARATOR + enc.encodeToString(SALT);
+        PasswordService passwordService = new DefaultPasswordService();
+        return passwordService.encryptPassword(password);
     }
 }
