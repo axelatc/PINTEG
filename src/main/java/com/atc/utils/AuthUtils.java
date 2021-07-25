e3aa85ed38479d8be93937970970877615ff26d5
axelatc <axel.krebs@promsocatc.net>
2021-07-25 17:53:50 +0200

    Remove unused method

--- src/main/java/com/atc/utils/AuthUtils.java
+++ src/main/java/com/atc/utils/AuthUtils.java
@@ -3,9 +3,6 @@ package com.atc.utils;
 import org.apache.log4j.Logger;
 import org.apache.shiro.authc.credential.DefaultPasswordService;
 import org.apache.shiro.authc.credential.PasswordService;
-import org.apache.shiro.env.BasicIniEnvironment;
-import org.apache.shiro.env.Environment;
-import org.apache.shiro.mgt.SecurityManager;
 
 /**
  * @author axel
@@ -21,16 +18,5 @@ public class AuthUtils {
     public final static String hashAndSalt(String password) {
         PasswordService passwordService = new DefaultPasswordService();
         return passwordService.encryptPassword(password);
-    }
-
-    /**
-     * @return the Security Manager configured by the shiro.ini
-     */
-    public static SecurityManager getSecurityManager() {
-        //source: https://stackoverflow.com/a/60139962
-        Environment env = new BasicIniEnvironment("classpath:shiro.ini");
-        SecurityManager securityManager = env.getSecurityManager();
-        //SecurityUtils.setSecurityManager(securityManager);
-        return securityManager;
     }
 }
