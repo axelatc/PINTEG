b367d5ec185ccb5d9cc3fe30b9a7016f90bc6dde
axelatc <axel.krebs@promsocatc.net>
2021-07-28 14:17:30 +0200

    Add authentication utils as a JSF managed bean

--- src/main/java/com/atc/utils/AuthUtils.java
+++ src/main/java/com/atc/utils/AuthUtils.java
@@ -1,15 +1,29 @@
 package com.atc.utils;
 
 import org.apache.log4j.Logger;
+import org.apache.shiro.SecurityUtils;
 import org.apache.shiro.authc.credential.DefaultPasswordService;
 import org.apache.shiro.authc.credential.PasswordService;
+import org.apache.shiro.subject.Subject;
+
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Named;
+
 
 /**
  * @author axel
  */
+@RequestScoped
+@Named
 public class AuthUtils {
     private final static Logger LOG = Logger.getLogger(AuthUtils.class);
 
+    public Subject getSubject() {
+        return SecurityUtils.getSubject();
+    }
+    public String getUsername() {
+        return (String) SecurityUtils.getSubject().getPrincipal();
+    }
 
     /**
      * @param password the password in clear text
